6fea430a478fff36730481f36edf3a7c8ef351af
jagadee93 <jagadeeshg1000@gmail.com>
2025-10-14 00:23:02 +0530

    Added CRUD operations for Category module; implemented Toaster notifications and Partial Views

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,13 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
 namespace Bulky.Models
 {
     public class Category
     {
        
         public int Id { get; set; }
-        [Required]
+
+        [Required,Display(Name="Category Name"),MinLength(2,ErrorMessage ="length should be at least 2"),MaxLength(10,ErrorMessage ="Name should be less than 10 characters")]
         public string Name { get; set; }
+        [Required,Display(Name ="Display Orderid"),Range(1,100,ErrorMessage ="Display order must be between 1 to 100")]
         public int DisplayOrder { get; set; }
     }
 }
