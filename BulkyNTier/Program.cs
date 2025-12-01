b54037b9bb750c5a3e0d0b6fff0b7812c7fd44f8
jagadee93 <jagadeeshg1000@gmail.com>
2025-12-01 23:25:09 +0530

    Added support for Identity Razor Pages by configuring routing and mapping in Program.cs

--- BulkyNTier/Program.cs
+++ BulkyNTier/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 //Add db Context
 
@@ -36,6 +37,9 @@ app.UseRouting();
 app.UseAuthentication();
 
 app.UseAuthorization();
+
+app.MapRazorPages();
+
 
 app.MapControllerRoute(
     name: "default",
